27bf725228f720a4774632bdc3b140becd5fff85
Sanjay R <77227213+SanJay1577@users.noreply.github.com>
2021-08-13 22:51:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+function validate(){
+    var name1 = document.querySelector("#validationDefault01");
+    var tab1 = document.querySelector(".data1");
+    tab1.innerText = name1.value;
+
+    var name2 = document.querySelector("#validationDefault02");
+    var tab2 = document.querySelector(".data2");
+    tab2.innerText = name2.value;
+
+    var name3 = document.querySelector("#validationDefault03");
+    var tab3 = document.querySelector(".data3");
+    tab3.innerText = name3.value;
+
+    var name4 = document.querySelector("#validationDefault04");
+    var tab4 = document.querySelector(".data4");
+    tab4.innerText = name4.value;
+
+    var name5 = document.querySelector("#validationDefault05");
+    var tab5 = document.querySelector(".data5");
+    tab5.innerText = name5.value;
+
+    var name6 = document.querySelector("#validationDefault06");
+    var tab6 = document.querySelector(".data6");
+    tab6.innerText = name6.value;
+
+    var name7 = document.querySelector("#validationDefault07");
+    var tab7 = document.querySelector(".data7");
+    tab7.innerText = name7.value;
+
+    var name8 = document.querySelector("#validationDefault08");
+    var tab8 = document.querySelector(".data8");
+    tab8.innerText = name8.value;
+    
+    var name9 = document.querySelector("#validationDefault09");
+    var tab9 = document.querySelector(".data9");
+    tab9.innerText = name9.value;
+}
